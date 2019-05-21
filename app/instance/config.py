b5090d6f3e2bca0256afe876d28c3ab067ba0d0a
t.abe <abeiru0921@gmail.com>
2019-05-21 23:51:05 +0900

    github登録

--- app/instance/config.py
+++ app/instance/config.py
@@ -1,6 +1,6 @@
 from os import urandom
 SECRET_KEY                     = urandom(24)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI        = 'postgresql://postgres:postgres@172.18.0.2/chatone'
+SQLALCHEMY_DATABASE_URI        = 'postgresql://postgres:admin@192.168.0.7:5432/chatone'
 DEBUG                          = False
 
